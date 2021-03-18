fcc8711ee1cbf2e145826e5712659a5de09817b9
nartesfasrum <nart@tirefire.pw>
2021-03-18 15:27:48 -0500

    urbot proof of concept
    can send messages from matrix to urbit, need to dedupe prior messages somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import configparser, quinnat, asyncio
-from nio import AsyncClient
+from nio import AsyncClient, RoomMessageText
 
 config = configparser.ConfigParser()
 config.read('default.ini')
@@ -13,20 +13,30 @@ urbitUrl = config['URBIT']['urbitUrl']
 urbitId = config['URBIT']['urbitId']
 urbitCode = config['URBIT']['urbitCode']
 urbitHost = config['URBIT']['urbitHost']
+urbitBridgeChat = config['URBIT']['urbitBridgeChat']
 
-async def main():
-    urbitClient = quinnat.Quinnat(urbitUrl, urbitId, urbitCode)
-    urbitClient.connect()
-    matrixClient = AsyncClient(matrixHomeServer, matrixBotUser)
-    print(await matrixClient.login(matrixBotPass))
+urbitClient = quinnat.Quinnat(urbitUrl, urbitId, urbitCode)
+
+async def urbitListener(message, replier):
     await matrixClient.room_send(
             room_id=matrixRoom,
             message_type="m.room.message",
             content={
                 "msgtype": "m.text",
-                "body": "ping"
+                "body": message.full_text
             }
     )
-    await matrixClient.close()
+
+async def matrixListener(room, event):
+    urbitClient.post_message(urbitHost, urbitBridgeChat, {"text": f"Got message from {room.user_name(event.sender)} in {room.display_name}: {event.body}"})
+
+async def main():
+    urbitClient.connect()
+
+    matrixClient = AsyncClient(matrixHomeServer, matrixBotUser)
+    print(await matrixClient.login(matrixBotPass))
+    matrixClient.add_event_callback(matrixListener, RoomMessageText)
+
+    await matrixClient.sync_forever(timeout=30000)
 
 asyncio.get_event_loop().run_until_complete(main())
