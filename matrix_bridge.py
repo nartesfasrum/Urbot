69cb5b0f7a015c4e21cfaf93d2cbe73e5785a326
nartesfasrum <nart@tirefire.pw>
2021-12-11 20:38:07 -0600

    minimum viable encryption product

--- matrix_bridge.py
+++ matrix_bridge.py
@@ -1,136 +1,117 @@
-import asyncio, os, quinnat
-from base_bridge import *
+import asyncio, json, os, sys, quinnat
 from helpers import get_json_dump
 from multiprocessing import active_children, Process
-from nio import AsyncClient, ClientConfig, MatrixRoom, RoomMessageText
+from nio import AsyncClient, ClientConfig, InviteEvent, LoginResponse, LocalProtocolError, MatrixRoom, MatrixUser, RoomMessageText, crypto, exceptions, RoomSendResponse
 
-class matrix_client(generic_bridge):
-    def __init__(self, instance, urb_info):
-        super().__init__(instance)
-        print("initialising matrix_client...")
-        self.urb_info = urb_info
-        self.instance = instance
-        self.bot_user = instance["matrix_bot_user"]
-        self.bot_password = instance["matrix_bot_pass"]
-        self.device_id = instance["matrix_device_id"]
-        self.homeserver = instance["matrix_homeserver"]
-        self.store_path = instance["matrix_store_path"]
-        self.channel_list = []
-        self.config = ClientConfig(store_sync_tokens=True, encryption_enabled=True)
-        self.matrixClient = AsyncClient(self.homeserver, self.bot_user, device_id=self.device_id, store_path=self.store_path, config=self.config)
+class MatrixClient(AsyncClient):
+    def __init__(self, homeserver, user='', device_id='', store_path='', config=None, ssl=None, proxy=None, password='', session_details_file='matrix_credentials_cache.json'):
+        super().__init__(homeserver, user=user, device_id=device_id, store_path=store_path, config=config, ssl=ssl, proxy=proxy)
+        if store_path and not os.path.isdir(store_path):
+            os.mkdir(store_path)
 
-        for channel_group in instance["channels"]:
-            self.channel_list.append(channel_group["matrix_room"])
+        self.session_details_file = session_details_file
+        self.password = password
 
-    async def send(self, room, body):
-        await self.matrixClient.room_send(
-            room_id = room,
-            message_type="m.room.message",
-            content={
-                "msgtype": "m.text",
-                "body": body
-            }
-        )
-        print("message sent!")
-
-class urbit_client:
-    def __init__(self, instance):
-        self.instance = instance
-        self.connect()
-
-    def connect(self):
-        self.client = quinnat.Quinnat(
-                self.instance["urbit_url"],
-                self.instance["client_ship"],
-                self.instance["urbit_code"]
-        )
-        print("connecting urbit_client...")
-        self.client.connect()
-
-    def send(self, resource_ship, channel, message):
-        try:
-            self.client.post_message(
-                    resource_ship,
-                    channel,
-                    {"text": message}
-            )
-        except UnicodeDecodeError:
-            self.reconnect()
-
-    def reconnect(self):
-        self.client.ship.delete()
-        self.client = self.connect()
-
-class bridge():
-    def __init__(self, instance, urb_info, matrix_client):
-        self.instance = instance
-        self.urbit_client = urbit_client(urb_info)
-        self.urb_info = urb_info
-        self.matrix_client = matrix_client
-
-    def start(self):
-        for channel in self.instance["channels"]:
-            self.urbit_client.send(channel['resource_ship'], channel['urbit_channel'], "Urbot has connected to this channel!")
-        async def urbit_message_handler(message, _):
-            matched_ships = list(filter(lambda ship: ship["resource_ship"] == message.host_ship, self.instance["channels"]))
-            if len(matched_ships) > 0:
-                for matched_ship in matched_ships:
-                    if matched_ship["urbit_channel"] == message.resource_name:
-                        message_body = message.author + ": " + message.full_text
-                        print("attempting to send message to Matrix...")
-                        print("message to be sent: ", message_body)
-                        print("message to room: ", matched_ship["matrix_room"])
-
-        def urbit_listener(message, _):
-            asyncio.run(urbit_message_handler(message, _))
-
-        while True:
+    async def login(self):
+        if os.path.exists(self.session_details_file) and os.path.isfile(self.session_details_file):
             try:
-                print("starting urbit listener...")
-                self.urbit_client.client.listen(urbit_listener)
-            except UnicodeDecodeError:
-                self.urbit_client.reconnect()
-                continue
+                with open("matrix_credentials_cache.json", "r") as f:
+                    config = json.load(f)
+                    self.access_token = config['access_token']
+                    self.user_id = config['user_id']
+                    self.device_id = config['device_id']
 
+                    self.load_store()
+                    print(f"Logged in using stored credentials: {self.user_id} on {self.device_id}")
 
-    async def matrix_message_handler(self, room: MatrixRoom, event: RoomMessageText):
-        matched_channels = list(filter(lambda channel: channel["matrix_room"] == room.machine_name, self.instance["channels"]))
-        for matched_channel in matched_channels:
-            message_body = room.user_name(event.sender) + ": " + event.body
-            print("attempting to send message to Urbit...")
-            print("message to be sent: ", message_body)
-            print("message to channel:", matched_channel["urbit_channel"])
-            self.urbit_client.send(matched_channel["resource_ship"], matched_channel["urbit_channel"], message_body)
+            except IOError as err:
+                print(f"Couldn't load session from file. Logging in. Error: {err}")
+            except json.JSONDecodeError:
+                print("Couldn't read JSON file! Overwriting...")
+
+        if not self.user_id or not self.access_token or not self.device_id:
+            response = await super().login(self.password)
+
+            if isinstance(response, LoginResponse):
+                print("Logged in using a password. Saving credentials to disk...")
+                self.__write_details_to_disk(response)
+            else:
+                print(f"Failed to log in! Response: {resp}")
+                sys.exit(1)
+
+    async def message_send(self, room, body):
+        try:
+            await self.room_send(
+                room_id = room,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": body
+                }
+            )
+            print("message sent!")
+        except exceptions.OlmUnverifiedDeviceError as err:
+            print("Known devices: ")
+            device_store: crypto.DeviceStore = device_store
+            [print(f"\t{device.user_id}\t {device.device_id}\t {device.trust_state}\t  {device.display_name}") for device in device_store]
+            sys.exit(1)
+
+    def cb_autojoin_room(self, room: MatrixRoom, event: InviteEvent):
+        print(f"Joining invited room {room.name}...")
+        self.join(room.room_id)
+        room = self.rooms[ROOM_ID]
+        print(f"Is {room.name} encrypted: {room.encrypted}")
+
+    async def cb_message_text(self, room: MatrixRoom, event: RoomMessageText):
+        if event.decrypted:
+            encrypted_symbol = "e "
+        else:
+            encrypted_symbol = "u "
+        print(f"{room.display_name} | {encrypted_symbol}| {room.user_name(event.sender)}: {event.body}")
+
+    @staticmethod
+    def __write_details_to_disk(resp: LoginResponse):
+
+        with open("matrix_credentials_cache.json", "w") as f:
+            json.dump({
+                "access_token": resp.access_token,
+                "device_id": resp.device_id,
+                "user_id": resp.user_id
+            }, f)
+
+async def run_matrix_client(client: MatrixClient):
+    await client.login()
+
+    async def after_first_sync():
+        print("Awaiting sync...")
+        await client.synced.wait()
+
+    after_first_sync_task = asyncio.create_task(after_first_sync())
+
+    sync_forever_task = asyncio.ensure_future(client.sync_forever(30000, full_state=True))
+
+    await asyncio.gather(
+        after_first_sync_task,
+        sync_forever_task
+    )
 
 async def main():
-    procs = []
-
     for instance in get_json_dump("config.json"):
-        print("dumping json...")
-        urb_info = {
-                "urbit_url": instance["urbit_url"],
-                "client_ship": instance["client_ship"],
-                "urbit_code": instance["urbit_code"]
-                }
-
         for bot in instance["bots"]:
-            if bot["type"] == "matrix":
-                print("setting up bots...")
-                matrix_instance = matrix_client(bot, urb_info)
-                bridge_instance = bridge(bot, urb_info, matrix_instance)
-            else:
-                raise Exception("bot type not implemented!")
+            config = ClientConfig(store_sync_tokens=True)
+            matrix_client = MatrixClient(bot["matrix_homeserver"], bot["matrix_bot_user"], store_path=bot["matrix_store_path"], config=config, password=bot["matrix_bot_pass"])
+            matrix_client.add_event_callback(matrix_client.cb_autojoin_room, InviteEvent)
+            matrix_client.add_event_callback(matrix_client.cb_message_text, RoomMessageText)
 
-            print("starting processes...")
-            response = await matrix_instance.matrixClient.login(matrix_instance.bot_password)
-            print(response)
-            bridge_proc = Process(target=bridge_instance.start)
+            try:
+                await run_matrix_client(matrix_client)
+            except (asyncio.CancelledError, KeyboardInterrupt):
+                await matrix_client.close()
 
-            bridge_proc.start()
-            procs.append(bridge_proc)
-    
-    matrix_instance.matrixClient.add_event_callback(bridge_instance.matrix_message_handler, RoomMessageText)
-
-    await matrix_instance.matrixClient.sync_forever(timeout=30000, full_state=True)
-
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(
+            main()
+        )
+    except KeyboardInterrupt:
+        pass
